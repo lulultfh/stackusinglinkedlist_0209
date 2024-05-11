f928c44cba8dd83c621b5823d86abfc1c6f785ff
lulultfh <punyalulu05@gmail.com>
2024-05-11 11:50:04 +0700

    add decision making if else in void peek

--- Stack_using_SingleLinkedList.cpp
+++ Stack_using_SingleLinkedList.cpp
@@ -46,6 +46,15 @@ public:
 
     //peek/top operation: retrieve the value of the topmost element without removing it
     void peek() {
-        
+        if (top == NULL) {
+            cout << "List is empty." << endl;
+        }
+        else {
+            Node* current = top;
+            while (current != NULL) {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+        }
     }
 }
