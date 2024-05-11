e64c2ab96386b4523fe46854da4dbf2d0ec028dc
lulultfh <punyalulu05@gmail.com>
2024-05-11 11:38:53 +0700

    add step 3 in void pop

--- Stack_using_SingleLinkedList.cpp
+++ Stack_using_SingleLinkedList.cpp
@@ -38,7 +38,8 @@ public:
     void pop() 
     {
         if (isEmpty()) { //step 1
-			cout << "Stack is empty." //step 1a
+			cout << "Stack is empty."; //step 1a
 		}
         cout << "Popped value: " <<top->data << endl; //step2
-};
+        top = top->next; //step 3: update the top pointer to the next node
+}
