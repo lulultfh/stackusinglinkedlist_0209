0c90fcbc921773b2e9750674466a72932cebfa32
lulultfh <punyalulu05@gmail.com>
2024-05-11 11:14:14 +0700

    add step 1 in function push

--- Stack_using_SingleLinkedList.cpp
+++ Stack_using_SingleLinkedList.cpp
@@ -26,6 +26,6 @@ public:
     //push operation: insert an element onto the top of the stack
     int push(int value)
     {
-        
+        Node* newNode = new Node(); //1. allocate memory for the new node
     }
 };
